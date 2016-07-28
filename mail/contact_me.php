f503e0691252326c87dbda539e3ab74357343108
Dan C <daniel.c4@gmail.com>
2016-07-28 14:40:16 -0300

    alter mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,7 +19,7 @@ $message = $_POST['message'];
 $to = 'sunsetstudio@sunsetstudio.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@sunsetstudio.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: sunsetstudio@sunsetstudio.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address\n";
 $headers .= "Return-Path: " . $to;
 if mail($to,$email_subject,$email_body,$headers)
