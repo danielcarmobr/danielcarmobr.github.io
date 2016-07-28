aec779401e433837669a231706dc1f8c88a69dc8
Dan C <daniel.c4@gmail.com>
2016-07-28 11:47:13 -0300

    fix height logo

--- js/contact_me.js
+++ js/contact_me.js
@@ -49,7 +49,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", parece que meu servidor de e-mail não está funcionando. Por favor tente novamnete mais tarde!");
+                    $('#success > .alert-danger').append("<strong>Desculpe " + firstName + ", parece que meu servidor de e-mail não esta funcionando. Por favor tente novamente mais tarde!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
