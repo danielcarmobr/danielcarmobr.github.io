8c499a64343ef446d8467a319f944551393ec066
Dan C <daniel.c4@gmail.com>
2016-07-28 15:38:21 -0300

    alter logo dim metod

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,12 +16,12 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$to = 'sunsetstudio@sunsetstudio.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'contato@sunsetstudio.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: sunsetstudio@sunsetstudio.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address\n";
-$headers .= "Return-Path: " . $to;
+$headers .= "Return-Path: sunsetstudio@sunsetstudio.com.br\n";
+$headers .= "Reply-To: $email_address";
 if mail($to,$email_subject,$email_body,$headers)
   return true;
 
